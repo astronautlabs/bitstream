511f8ea871b4e3d9f0c8b904e412f9ff51669224
William Lahti <wilahti@gmail.com>
2020-10-25 01:29:43 -0700

    strengthen basic reader tests

--- src/reader.test.ts
+++ src/reader.test.ts
@@ -3,7 +3,7 @@ import { describe } from "razmin";
 import { BitstreamReader } from "./reader";
 
 describe('BitstreamReader', it => {
-    it('works', () => {
+    it('can correctly deserialize a simple example from a single buffer', () => {
         let bitstream = new BitstreamReader();
 
         bitstream.addBuffer(Buffer.from([
@@ -17,13 +17,32 @@ describe('BitstreamReader', it => {
 
         expect(bitstream.readSync(1)).to.equal(0b1);
         expect(bitstream.readSync(3)).to.equal(0b100);
-        expect(bitstream.readSync(4)).to.equal(0b1000);
+        expect(bitstream.readSync(5)).to.equal(0b10000);
 
-        expect(bitstream.readSync(2)).to.equal(0b01);
+        expect(bitstream.readSync(1)).to.equal(0b1);
         expect(bitstream.readSync(5)).to.equal(0b01010);
         expect(bitstream.readSync(1)).to.equal(0b0);
 
         expect(bitstream.readSync(8)).to.equal(0b11101001);
+
+        expect(bitstream.readSync(11)).to.equal(0b01100100100);
+        expect(bitstream.readSync(5)).to.equal(0b01110);
+    });
+    it('can correctly deserialize a simple example from multiple buffers', () => {
+        let bitstream = new BitstreamReader();
+
+        bitstream.addBuffer(Buffer.from([ 0b11001000, 0b01010100 ]));
+        bitstream.addBuffer(Buffer.from([ 0b11101001, 0b01100100, 0b10001110 ]));
+
+        expect(bitstream.readSync(1)).to.equal(0b1);
+        expect(bitstream.readSync(3)).to.equal(0b100);
+        expect(bitstream.readSync(4)).to.equal(0b1000);
+
+        expect(bitstream.readSync(2)).to.equal(0b01);
+        expect(bitstream.readSync(5)).to.equal(0b01010);
+        expect(bitstream.readSync(2)).to.equal(0b01);
+
+        expect(bitstream.readSync(7)).to.equal(0b1101001);
 
         expect(bitstream.readSync(11)).to.equal(0b01100100100);
         expect(bitstream.readSync(5)).to.equal(0b01110);
